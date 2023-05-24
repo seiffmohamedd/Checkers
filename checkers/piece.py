1aca897e3820a913de61d55f343592c3a4acac8c
A7madAdelll <ahmadaa145632@gmail.com>
2023-05-24 10:04:20 +0300

    piece class added

---
+++ checkers/piece.py
@@ -0,0 +1,37 @@
+from .constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
+import pygame
+
+class Piece:
+    PADDING = 15
+    OUTLINE = 2
+
+    def __init__(self, row, col, color): #constructor 
+        self.row = row
+        self.col = col
+        self.color = color
+        self.king = False
+        self.x = 0
+        self.y = 0
+        self.calc_pos()
+
+    def calc_pos(self):  #calculate the position of the piece
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+    def make_king(self):  #piece became a king when it reach the other side of the board
+        self.king = True
+    
+    def draw(self, win):
+        radius = SQUARE_SIZE//2 - self.PADDING
+        pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE) # CIRLCE As a Piece the bigger one includes the small one        
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2)) #the crown picture is dispalyed on the piece if the piece became a king
+
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calc_pos()
+
+    def __repr__(self):
+        return str(self.color)
